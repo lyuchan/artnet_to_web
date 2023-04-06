c5a828c624f07035de182fe9aa6f40481c94feca
BuildTools <unconfigured@null.spigotmc.org>
2023-04-07 00:22:06 +0800

    add dimmer

--- run.js
+++ run.js
@@ -6,7 +6,15 @@ var receiver = dmxnet.newReceiver({
     universe: 1,
     net: 0,
 });
-
+let dimmer_address = 1
+let r_address = 2
+let g_address = 3
+let b_address = 4
+let shutter_address = 5
+let old_r = 0
+let old_g = 0
+let old_b = 0
+let old_s = 0
 const app = express();
 const SocketServer = require("ws").Server;
 const server = app.listen(8080, () => {
@@ -26,7 +34,22 @@ wss.on("connection", (ws) => {
 });
 receiver.on('data', function (data) {
     //console.log(data)
-    send(JSON.stringify({ data: data }))
+    let dimmer = data[dimmer_address - 1]
+    let r = data[r_address - 1] * (dimmer / 255)
+    let g = data[g_address - 1] * (dimmer / 255)
+    let b = data[b_address - 1] * (dimmer / 255)
+    let shutter = data[shutter_address - 1]
+    // console.log(JSON.stringify({ r: r, g: g, b: b }))
+
+    if (old_r != r || old_g != g || old_b != b || old_s != shutter) {
+        send(JSON.stringify({ r: r, g: g, b: b, s: shutter }))
+        //console.log(JSON.stringify({ r: r, g: g, b: b }))
+        old_r = r;
+        old_g = g;
+        old_b = b;
+        old_s = shutter;
+    }
+
 });
 function send(data) {
     let clients = wss.clients;
