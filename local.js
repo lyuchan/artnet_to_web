0bad8786b9661b6f5c3dd73732587202d6bc2e72
lyuchan <david12312330@gmail.com>
2023-04-12 11:54:41 +0800

    use_ws

--- local.js
+++ local.js
@@ -1,4 +1,5 @@
-const http = require('http');
+const WebSocket = require('ws');
+const socket = new WebSocket('ws://light.henrywu.tw:3000');
 var dmxlib = require('dmxnet');
 var dmxnet = new dmxlib.dmxnet({});
 var receiver = dmxnet.newReceiver({
@@ -15,44 +16,40 @@ let old_r = 0
 let old_g = 0
 let old_b = 0
 let old_s = 0
-let serverip="light.henrywu.tw:3000"
+let serverip = "light.henrywu.tw:3000"
+socket.on('open', () => {
+    console.log('已连接到WebSocket服务器');
 
-receiver.on('data', function (data) {
-    
-    let dimmer = data[dimmer_address - 1]
-    let r = data[r_address - 1] * (dimmer / 255)
-    let g = data[g_address - 1] * (dimmer / 255)
-    let b = data[b_address - 1] * (dimmer / 255)
-    let shutter = data[shutter_address - 1]
-    // console.log(JSON.stringify({ r: r, g: g, b: b }))
-   // console.log(r,g,b)
-    if (old_r != r || old_g != g || old_b != b || old_s != shutter) {
-        send(serverip,r,g,b,shutter)
-        //console.log(JSON.stringify({ r: r, g: g, b: b }))
-        old_r = r;
-        old_g = g;
-        old_b = b;
-        old_s = shutter;
-        
-    }
+    // 向服务器发送消息
 
-});
-function send(ip,r, g, b, s) {
-    // 始終檢查引數是否存在，如果不存在，則設置為預設值
-    r = r || 0;
-    g = g || 0;
-    b = b || 0;
-    s = s || 0;
-  
-    // 構建 URL 字串
-    const url = `http://${ip}/update?token=password&r=${r}&g=${g}&b=${b}&s=${s}`;
-  
-    // 發送 HTTP GET 請求
-    http.get(url, (res) => {
-     // console.log(`已發送請求至：${url}`);
-      // 在此處理回應
-      // ...
-    }).on('error', (err) => {
-     // console.error(`請求失敗：${err.message}`);
+
+    receiver.on('data', function (data) {
+
+        let dimmer = data[dimmer_address - 1]
+        let r = data[r_address - 1] * (dimmer / 255)
+        let g = data[g_address - 1] * (dimmer / 255)
+        let b = data[b_address - 1] * (dimmer / 255)
+        let shutter = data[shutter_address - 1]
+        // console.log(JSON.stringify({ r: r, g: g, b: b }))
+
+        if (old_r != r || old_g != g || old_b != b || old_s != shutter) {
+            socket.send(JSON.stringify({ t: "password", d: d, r: r, g: g, b: b, s: shutter }));
+            //console.log(JSON.stringify({ r: r, g: g, b: b }))
+            old_r = r;
+            old_g = g;
+            old_b = b;
+            old_s = shutter;
+
+        }
+
     });
-  }
+});
+socket.on('message', (message) => {
+    //console.log(`接收到服务器消息：${message}`);
+});
+
+// 监听关闭事件
+socket.on('close', () => {
+    console.log('已关闭WebSocket连接');
+});
+
